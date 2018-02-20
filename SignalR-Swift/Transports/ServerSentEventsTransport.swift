7582baf4af2eaf7cc73cc6c791e0c6fadfa747e4
Vladimir Kushelkov <vldalxnd@gmail.com>
2018-02-20 11:03:48 +0300

    refactoring

--- SignalR-Swift/Transports/ServerSentEventsTransport.swift
+++ SignalR-Swift/Transports/ServerSentEventsTransport.swift
@@ -150,9 +150,8 @@ public class ServerSentEventsTransport: HttpTransport {
     
     private func reconnect(connection: ConnectionProtocol, data: String?) {
         _ = BlockOperation { [weak self, weak connection] in
-            guard let strongSelf = self, let strongConnection = connection else { return }
-            
-            if strongConnection.state != .disconnected && Connection.ensureReconnecting(connection: strongConnection) {
+            if let strongSelf = self, let strongConnection = connection,
+               strongConnection.state != .disconnected, Connection.ensureReconnecting(connection: strongConnection) {
                 strongSelf.open(connection: strongConnection, connectionData: data, isReconnecting: true)
             }
         }.perform(#selector(BlockOperation.start), with: nil, afterDelay: self.reconnectDelay)
