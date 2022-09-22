78df753aab8740434947941cbdb1f0d83a262216
Kamran Khan <kamrankhan0787@gmail.com>
2022-09-22 07:55:15 +0700

    update pacakge swift file

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SignalRSwift",
-            dependencies: [],
+            dependencies: ["Alamofire", "Starscream"],
             path: "SignalR-Swift"),
         .testTarget(
             name: "SignalRSwiftTests",
